bf5a9b8e51280d0f9814601b85789aba87a4cd4c
flojoho <hofkas.flo@gmx.at>
2026-07-29 21:14:29 +0200

    draw bounding box

--- src/lib/canvasLogic.ts
+++ src/lib/canvasLogic.ts
@@ -1,15 +1,10 @@
 
 const fps = 30;
 
-const sideLength = 100;
+const padding = 100;
 
 const startAnimation = (canvas: HTMLCanvasElement) => {
   const ctx = canvas.getContext('2d')!;
-
-  let x = 0;
-  let y = 0;
-  let vx = 5;
-  let vy = 5;
 
   function resize() {
     canvas.width = canvas.clientWidth;
@@ -25,17 +20,22 @@ const startAnimation = (canvas: HTMLCanvasElement) => {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = 'white';
-    ctx.lineWidth = 1;
-    ctx.fillRect(x, y, sideLength, sideLength);
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 2;
 
-    if(x > canvas.width - sideLength && vx > 0) vx = -vx;
-    if(x < 0 && vx < 0) vx = -vx;
-    if(y > canvas.height - sideLength && vy > 0) vy = -vy;
-    if(y < 0 && vy < 0) vy = -vy;
+    ctx.moveTo(padding, padding);
+    ctx.lineTo(canvas.width - padding, padding);
 
-    x += vx;
-    y += vy;
+    ctx.moveTo(padding, canvas.height - padding);
+    ctx.lineTo(canvas.width - padding, canvas.height - padding);
+
+    ctx.moveTo(padding, padding);
+    ctx.lineTo(padding, canvas.height - padding);
+
+    ctx.moveTo(canvas.width - padding, padding);
+    ctx.lineTo(canvas.width - padding, canvas.height - padding);
+
+    ctx.stroke();
   }, 1000/fps);
 
   return {
